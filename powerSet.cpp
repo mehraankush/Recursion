078845e778e0858770a07860ff4a27b85cd615f6
Ankush mehra <mehraankush36@gmail.com>
2024-01-22 11:38:32 +0530

    Update powerSet.cpp

--- powerSet.cpp
+++ powerSet.cpp
@@ -17,3 +17,29 @@
 
    return res;
  }
+
+// Backtracking 
+
+   void generateSubsets(vector<int>& nums,int index ,vector<int>& current, vector<vector<int>>&res){
+        if (index == nums.size()) {
+            res.push_back(current);
+            return;
+        }
+        // Exclude the current element
+        generateSubsets(nums, index + 1, current, res);
+
+        // Include the current element
+        current.push_back(nums[index]);
+        generateSubsets(nums, index + 1, current, res);
+        current.pop_back();
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+       vector<vector<int>>res;
+       int n = nums.size();
+       vector<int>temp;
+  
+      generateSubsets(nums,0,temp,res);
+      return res;
+        
+    }
