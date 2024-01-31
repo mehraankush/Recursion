f50b5ac57df8f0d913c8d809f84cec1bcd2f55fd
Ankush mehra <mehraankush36@gmail.com>
2024-02-01 00:25:33 +0530

    Create SubSetSum.cpp

---
+++ SubSetSum.cpp
@@ -0,0 +1,38 @@
+
+
+int FindSum(vector<int>&arr){
+     int temp = 0;
+	for(int i=0;i<arr.size();i++){
+       temp+=arr[i];
+	}
+
+	return temp;
+}
+
+void findSubsetSum(vector<int> &arr,vector<int>&res,vector<int>temp,int ind){
+	if(ind==arr.size()){
+		if(temp.size()!=0){
+           int sum = FindSum(temp);
+		   res.push_back(sum);
+		}else{
+		 res.push_back(0);
+		}
+		return;
+	}
+	temp.push_back(arr[ind]);
+	findSubsetSum(arr,res,temp,ind+1);
+	temp.pop_back();
+
+	findSubsetSum(arr,res,temp,ind+1);
+}
+
+
+vector<int> subsetSum(vector<int> &num){
+	// Write your code here.
+	vector<int>res;
+	vector<int>temp;
+	findSubsetSum(num,res,temp,0);
+	sort(res.begin(),res.end());
+	return res;
+		
+}
